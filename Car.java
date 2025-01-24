6878c915ea1aa8fdf4acc4f6a264f3ed847da906
Pontus Gustafsson <pontusgustafsson@MacBook-Pro-som-tillhor-Pontus.local>
2025-01-24 10:35:26 +0100

    ny superklass Car som volvo och saab ärver ifrån

---
+++ Car.java
@@ -0,0 +1,49 @@
+/*Eftersom klasserna Saaab95 och Volvo240 delar många egenskaper och metoder är implementationsarv att föredra för att minska dupplicering av kod. */
+
+import java.awt.*;
+public class Car {
+    
+    public int nrDoors; // Number of doors on the car
+    public double enginePower; // Engine power of the car
+    public double currentSpeed; // The current speed of the car
+    public Color color; // Color of the car
+    public String modelName; // The car model name
+    
+    public Car(int Doors,double power, Color paint, String model){
+        nrDoors=Doors;
+        enginePower=power;
+        color = paint;
+        modelName = model;
+        stopEngine();
+    }
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+	    color = clr;
+    }
+
+    public void startEngine(){
+	    currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+	    currentSpeed = 0;
+    }
+    public double speedFactor(){
+        return enginePower*0.01;
+    }
+    
+}
